90ae8edddf528017cdc1abf9f413677a26842d03
Daniel Ancines <daniel.ancines@gmail.com>
2026-09-23 14:02:50 -0300

    Initial Commit

---
+++ SwiftUI.Metronome/SwiftUI_MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MetronomeApp.swift
+//  SwiftUI.Metronome
+//
+//  Created by Daniel Ancines on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI.MetronomeTests/SwiftUI_MetronomeTests.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_MetronomeTests.swift
+//  SwiftUI.MetronomeTests
+//
+//  Created by Daniel Ancines on 23/09/26.
+//
+
+import Testing
+
+struct SwiftUI_MetronomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
